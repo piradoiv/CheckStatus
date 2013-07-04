bb144778230c7657d12ecdb0a012f0d4409817eb
Ricardo Cruz <piradoiv@gmail.com>
2013-07-04 13:54:35 +0100

    Add first tests

--- tests/CheckStatusTest.php
+++ tests/CheckStatusTest.php
@@ -4,11 +4,23 @@ class CheckStatusTest extends PHPUnit_Framework_TestCase
 {
   public function setUp()
   {
+    $this->url = 'http://localhost/';
   }
 
   public function testComposerIsLoaded()
   {
     $curl = new Curl();
     $this->assertInstanceOf('Curl', $curl, "Try running composer install");
+  }
+
+  public function testCanFetchUrl()
+  {
+    $checker = new CheckStatus\CheckStatus();
+    $response = $checker->fetchUrl('http://localhost/');
+
+    $this->assertNotNull($response);
+    $this->assertInstanceOf('CheckStatus\Status', $response);
+    $this->assertNotNull($response->getCode());
+    $this->assertEquals(200, $response->getCode());
   }
 }
