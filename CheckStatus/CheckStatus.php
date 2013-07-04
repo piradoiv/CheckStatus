e043789ecfcb1d49c108e5791a85f792435e6145
Ricardo Cruz <piradoiv@gmail.com>
2013-07-04 14:15:55 +0100

    Add response time

--- CheckStatus/CheckStatus.php
+++ CheckStatus/CheckStatus.php
@@ -6,9 +6,11 @@ class CheckStatus
 {
   public function fetchUrl($url = null)
   {
+    $initTime = microtime();
     $curl = new \Curl();
     $response = $curl->get($url);
     $status = new Status($response);
+    $status->setResponseTime(microtime() - $initTime);
 
     return $status;
   }
