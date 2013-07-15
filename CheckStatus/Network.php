cf8d21aaaf7047eb25147f8192765a12c6f13f01
Ricardo Cruz <piradoiv@gmail.com>
2013-07-15 10:48:18 +0100

    Add empty Network class

---
+++ CheckStatus/Network.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * CheckStatus / CheckStatus
+ * =========================
+ * This is a library to check the status of a website, it returns if the
+ * response is successful, the response time, etc.
+ *
+ * @package CheckStatus
+ * @author  Ricardo Cruz <piradoiv@gmail.com>
+ * @link    https://github.com/piradoiv/CheckStatus/
+ * @since   1.3.0
+ */
+
+namespace CheckStatus;
+
+/**
+ * Network class
+ *
+ * This is the class on charge to check if the network is currently available
+ * or not.
+ */
+class Network
+{
+  public $available;
+
+}
