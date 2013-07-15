f59c9a3e63a34828f8d23b537057f1ed14139587
Ricardo Cruz <piradoiv@gmail.com>
2013-07-15 14:10:25 +0100

    Add comments to Network

--- CheckStatus/Network.php
+++ CheckStatus/Network.php
@@ -21,11 +21,44 @@ namespace CheckStatus;
  */
 class Network
 {
+  /**
+   * Network availability status
+   *
+   * @var boolean $available
+   */
   public $available;
+
+  /**
+   * Last timestamp this class checked
+   * network availability
+   *
+   * @var double $lastCheck
+   */
   public $lastCheck;
+
+  /**
+   * Seconds we will wait until check the
+   * status again
+   *
+   * @var double $recheckAfter
+   */
   public $recheckAfter;
+
+  /**
+   * The test URL we know it's always online,
+   * like Google, Yahoo, Amazon, ...
+   *
+   * @var string $testUrl
+   */
   public $testUrl;
 
+  /**
+   * Constructor of the class, we can pass a
+   * true on first parameter to call $this->check()
+   * on load
+   *
+   * @param boolean $autocheck
+   */
   public function __construct($autocheck = true)
   {
     $this->recheckAfter = 60 * 5; // 5 minutes
@@ -36,6 +69,13 @@ class Network
     }
   }
 
+  /**
+   * This method calls checkNetwork() method if,
+   * and only if, we waited enough to avoid
+   * flooding the testing URL.
+   *
+   * @return boolean True if available, false otherwise
+   */
   public function check()
   {
     $currentTime = microtime(true);
@@ -49,7 +89,13 @@ class Network
     return $this->available;
   }
 
-  public function checkNetwork()
+  /**
+   * Checks the network status
+   *
+   * @return boolean True if available, false otherwise
+   * @private
+   */
+  private function checkNetwork()
   {
     $curl = new \Curl();
 
