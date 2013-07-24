f0374eca101d58f7ab092361ff548173a32b8847
Ricardo Cruz <piradoiv@gmail.com>
2013-07-24 13:50:09 +0100

    Initial updates on classes to use internal Curl

--- CheckStatus/Curl.php
+++ CheckStatus/Curl.php
@@ -27,10 +27,53 @@ class Curl
   public $handler;
 
   /**
+   * User Agent to use in requests
+   *
+   * @var string $userAgent
+   */
+  public $userAgent;
+
+  /**
    * Constructor of the class
    */
   public function __construct()
   {
+    $this->userAgent = 'Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/28.0.1468.0 Safari/537.36';
+  }
+
+  private function prepareHandler($url, $options = array())
+  {
+    $handler = curl_init($url);
+    $defaultOptions = array(
+      CURLOPT_AUTOREFERER => true,
+      CURLOPT_RETURNTRANSFER => true,
+      CURLOPT_MAXREDIRS => 5,
+      CURLOPT_USERAGENT => $this->userAgent,
+    );
+
+    $options = array_merge($defaultOptions, $options);
+
+    curl_setopt_array($handler, $options);
+
+    return $handler;
+  }
+
+  private function generalRequest($method, $url, $options = array())
+  {
+    if (strtolower($method) == 'post') {
+      $options[CURLOPT_POST] = true;
+    }
+
+    $handler = $this->prepareHandler($url, $options);
+    $html = curl_exec($handler);
+    $status = new Status($handler, $html);
+
+    return $status;
+  }
+
+  public function get($url, $options = array())
+  {
+    return $this->generalRequest('get', $url, $options);
   }
 
 }
