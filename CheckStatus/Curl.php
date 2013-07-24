6c71b54bf0cb787d8d396c4869839ae22e3a698f
Ricardo Cruz <piradoiv@gmail.com>
2013-07-24 13:17:16 +0100

    Add initial version of the Curl class

---
+++ CheckStatus/Curl.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * CheckStatus / Curl
+ * =========================
+ * Custom wrapper for PHP native Curl
+ *
+ * @package CheckStatus
+ * @author  Ricardo Cruz <piradoiv@gmail.com>
+ * @link    https://github.com/piradoiv/CheckStatus/
+ * @since   2.0.0
+ */
+
+namespace CheckStatus;
+
+/**
+ * Curl class
+ *
+ * Custom wrapper for PHP native Curl
+ */
+class Curl
+{
+  /**
+   * Curl handler
+   *
+   * @var resource $handler
+   */
+  public $handler;
+
+  /**
+   * Constructor of the class
+   */
+  public function __construct()
+  {
+  }
+
+}
---
+++ tests/CurlTest.php
@@ -0,0 +1,14 @@
+<?php
+
+class CurlTest extends PHPUnit_Framework_TestCase
+{
+  public function setUp()
+  {
+  }
+
+  public function testClassExists()
+  {
+    $curl = new CheckStatus\Curl();
+    $this->assertInstanceOf('CheckStatus\Curl', $curl, "Try running composer install");
+  }
+}
